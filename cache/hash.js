c944c68b9814dd7c9e4f3941759cae48304e0021
terp1s <piskova.ter@gmail.com>
2025-09-04 15:32:44 +0200

    ups spatny js

--- cache/hash.js
+++ cache/hash.js
@@ -1,8 +1,11 @@
-import crypto from 'crypto';
+const crypto = require('crypto');
 
 function AddHash(r){
-    let h = r.uri
-    var hash = crypto.createHash('md5').update(h).digest('hex');
+    let h = r.uri;
+    var hash = crypto.createHash('md5').update(r).digest('hex');
+    r.headersOut["x-Cache-Key"] = hash;
 }
 
 export default{AddHash}
+
+AddHash("/test/page");
