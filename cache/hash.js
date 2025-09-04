1f03e79e42b3617b514b8b1528dfbb35ed18177c
terp1s <piskova.ter@gmail.com>
2025-09-04 15:33:28 +0200

    jeste jednou spatny js

--- cache/hash.js
+++ cache/hash.js
@@ -2,7 +2,7 @@ const crypto = require('crypto');
 
 function AddHash(r){
     let h = r.uri;
-    var hash = crypto.createHash('md5').update(r).digest('hex');
+    var hash = crypto.createHash('md5').update(h).digest('hex');
     r.headersOut["x-Cache-Key"] = hash;
 }
 
